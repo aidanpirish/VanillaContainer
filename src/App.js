f98f74ec53f15d7a5aec426051cb0a93f53175bf
aidanpirish <aidanpirish@gmail.com>
2021-03-06 13:13:08 -0500

    various fixes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 
 function App() {
+  
   return (
     <div className="App">
       //display page title, dynamically create for each in folder
-      <iframe src="./test/index.html" title="hello"></iframe>
+      <iframe src="./sites/site1/index.html" title="hello"></iframe>
     </div>
   );
 }
